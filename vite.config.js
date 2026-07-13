6db6a42ae9421086f552a29b208b90ae208a8c0a
Roshan Jadhav <roshanjadhav2830@gmail.com>
2026-07-13 12:27:14 +0530

    Update base URL in Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chatbot-project/'
+  base: 'https://github.com/roshan-0608/ChatBot'
 })
